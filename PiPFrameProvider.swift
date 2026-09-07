0469d76d89ef1a41b3d88e8a5b5cb5e9aa636c8c
Phzin7zz0 <pedrohenriquer827@gmail.com>
2026-09-07 03:48:45 -0400

    Update PiPFrameProvider.swift

--- PiPFrameProvider.swift
+++ PiPFrameProvider.swift
@@ -1,34 +1,32 @@
 import Foundation
 import AVFoundation
 import CoreMedia
-
+ 
 final class PiPFrameProvider {
-
     private let displayLayer: AVSampleBufferDisplayLayer
     private var frameCount: Int64 = 0
-
+ 
     init(displayLayer: AVSampleBufferDisplayLayer) {
         self.displayLayer = displayLayer
     }
-
+ 
     func update(text: String) {
         guard let pixelBuffer = TimerRenderer.createPixelBuffer(text: text) else {
             return
         }
-
+ 
         var formatDescription: CMVideoFormatDescription?
-
         let status = CMVideoFormatDescriptionCreateForImageBuffer(
             allocator: kCFAllocatorDefault,
             imageBuffer: pixelBuffer,
             formatDescriptionOut: &formatDescription
         )
-
+ 
         guard status == noErr,
               let formatDescription = formatDescription else {
             return
         }
-
+ 
         var timingInfo = CMSampleTimingInfo(
             duration: CMTime(value: 1, timescale: 30),
             presentationTimeStamp: CMTime(
@@ -37,9 +35,8 @@ final class PiPFrameProvider {
             ),
             decodeTimeStamp: .invalid
         )
-
+ 
         var sampleBuffer: CMSampleBuffer?
-
         let result = CMSampleBufferCreateReadyWithImageBuffer(
             allocator: kCFAllocatorDefault,
             imageBuffer: pixelBuffer,
@@ -47,12 +44,12 @@ final class PiPFrameProvider {
             sampleTiming: &timingInfo,
             sampleBufferOut: &sampleBuffer
         )
-
+ 
         guard result == noErr,
               let sampleBuffer = sampleBuffer else {
             return
         }
-
+ 
         if displayLayer.isReadyForMoreMediaData {
             displayLayer.enqueue(sampleBuffer)
             frameCount += 1
